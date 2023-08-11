7e4c743a8e08f7d3ac840f915cfdc590015ad628
7216nat <chenxkv5@outlook.com>
2023-08-11 20:46:57 +0200

    typo fix

--- code/kuksa.py
+++ code/kuksa.py
@@ -4,9 +4,9 @@ import kuksa_viss_client
 
 
 def kuksa_ini():
-    config = {"cacertificate": "../kuksa_certificates/CA.pem",
-        "certificate": "../kuksa_certificates/Client.pem",
-        "key": "../kuksa_certificates/Client.key"}
+    config = {"cacertificate": "/etc/kuksa-val/CA.pem",
+        "certificate": "/etc/kuksa-val/Client.pem",
+        "key": "/etc/kuksa-val/Client.key"}
     client = kuksa_viss_client.KuksaClientThread(config)
     with open("../kuksa_certificates/super-admin.json.token", "r") as f:
         token = f.read()
